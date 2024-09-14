a9662dc87021ad2d39ce76036f6336a63680539c
Elliott Minns <elliott.minns@pm.me>
2024-09-14 14:19:30 -0500

    pure nix hyprland

--- nix/home/hyprland.nix
+++ nix/home/hyprland.nix
@@ -1,88 +1,90 @@
-{...}:
-{
+{...}: {
   enable = true;
   settings = {
     "$mod" = "SUPER";
-    input = {
-      monitor = [
-      ];
+    exec-once = ["ags"];
+    exec = [
+      ''gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"   # for GTK3 apps''
+      ''gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"   # for GTK4 apps''
+      ''gsettings set org.gnome.desktop.interface cursor-theme "Banana-Catppuccin-Mocha"''
+      ''gsettings set org.gnome.desktop.interface cursor-size 128''
+    ];
+    xwayland = {
+      force_zero_scaling = true;
     };
+    general = {
+      gaps_out = 30;
+    };
+    input = {
+      follow_mouse = 1;
+      touchpad = {
+        natural_scroll = true;
+        disable_while_typing = true;
+        tap-to-click = false;
+        middle_button_emulation = false;
+      };
+      sensitivity = 0;
+    };
+    env = [
+      "QT_QPA_PLATFORMTHEME,qt6ct"
+      "HYPRCURSOR_SIZE,128"
+      "XCURSOR_SIZE,128"
+    ];
+    monitor = [
+      "DP-1,3840x2160,1600x0,2"
+      "eDP-2,2560x1600,0x0,1.6"
+    ];
+    bezier = [
+      "easeOutBack,0.34,1.56,0.64,1"
+      "easeInBack,0.36,0,0.66,-0.56"
+      "easeInCubic,0.32,0,0.67,0"
+      "easeInOutCubic,0.65,0,0.35,1"
+    ];
+    animation = [
+      "windowsIn,1,5,easeOutBack,popin"
+      "windowsOut,1,5,easeInBack,popin"
+      "fadeIn,0"
+      "fadeOut,1,10,easeInCubic"
+      "workspaces,1,4,easeInOutCubic,slide"
+    ];
     bind =
-    [
-      "$mod, Return, exec, alacritty"
-      "$mod, Q, killactive"
-      "$mod, M, exit"
-      "$mod, F, exec, firefox"
-      "$mod, E, exec, nautilus"
-      "$mod, V, togglefloating"
-      "$mod, R, exec, wofi --show drun"
-      "$mod, P, pseudo"
-      "$mod, J, togglesplit"
-      "$mod, T, togglegroup"
-      "$mod+ALT, J, changegroupactive, f"
-      "$mod+ALT, K, changegroupactive, f"
-      "$mod, left, movefocus, l"
-      "$mod, right, movefocus, r"
-      "$mod, up, movefocus, u"
-      "$mod, down, movefocus, d"
-      "$mod, h, movefocus, l"
-      "$mod, l, movefocus, r"
-      "$mod, k, movefocus, u"
-      "$mod, j, movefocus, d"
-    ]
-    ++ (
-      builtins.concatLists (builtins.genList (
-        x: let
-          ws = let
-            c = (x + 1) / 10;
-          in
-            builtins.toString(x + 1 - (c * 10));
-        in [
-          "$mod, ${ws}, workspace, ${toString (x + 1)}"
-          "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-        ]
-      )
-      10)
-    );
+      [
+        "$mod, Return, exec, alacritty"
+        "$mod, Q, killactive"
+        "$mod, M, exit"
+        "$mod, F, exec, firefox"
+        "$mod, E, exec, nautilus"
+        "$mod, V, togglefloating"
+        "$mod, R, exec, wofi --show drun"
+        "$mod, P, pseudo"
+        "$mod, J, togglesplit"
+        "$mod, T, togglegroup"
+        "$mod+ALT, J, changegroupactive, f"
+        "$mod+ALT, K, changegroupactive, f"
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+        "$mod, h, movefocus, l"
+        "$mod, l, movefocus, r"
+        "$mod, k, movefocus, u"
+        "$mod, j, movefocus, d"
+      ]
+      ++ (
+        builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              "$mod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]
+          )
+          10)
+      );
   };
-
   extraConfig = ''
-  exec-once = ags
-  exec = gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"   # for GTK3 apps
-  exec = gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"   # for GTK4 apps
-  exec = gsettings set org.gnome.desktop.interface cursor-theme "Banana-Catppuccin-Mocha"
-  exec = gsettings set org.gnome.desktop.interface cursor-size 128
-
-  env = QT_QPA_PLATFORMTHEME,qt6ct   # for Qt apps
-  env = HYPRCURSOR_SIZE,128
-  env = XCURSOR_SIZE,128
-
-  bezier=easeOutBack,0.34,1.56,0.64,1
-  bezier=easeInBack,0.36,0,0.66,-0.56
-  bezier=easeInCubic,0.32,0,0.67,0
-  bezier=easeInOutCubic,0.65,0,0.35,1
-  animation=windowsIn,1,5,easeOutBack,popin
-  animation=windowsOut,1,5,easeInBack,popin
-  animation=fadeIn,0
-  animation=fadeOut,1,10,easeInCubic
-  animation=workspaces,1,4,easeInOutCubic,slide
-  general:gaps_out=30
-  xwayland {
-    force_zero_scaling = true
-  }
-
-  monitor=DP-1,3840x2160,1600x0,2
-  monitor=eDP-2,2560x1600,0x0,1.6
-
-  input {
-    follow_mouse = 1
-      touchpad {
-        natural_scroll = true
-          disable_while_typing = true
-          tap-to-click = false
-          middle_button_emulation = false
-      }
-    sensitivity = 0
-  }
   '';
 }
