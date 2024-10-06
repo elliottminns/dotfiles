0876ebfc86a34d606d626e0f2092579558bfd670
Elliott Minns <elliott.minns@pm.me>
2024-10-06 10:43:07 -0500

    nix updates

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -39,36 +39,39 @@
         inputs.templ.overlays.default
         add-unstable-packages
       ];
-      environment.systemPackages = [
-        pkgs.alacritty
-        pkgs.air
-        pkgs.awscli
-        pkgs.bun
-        pkgs.ffmpeg
-        pkgs.git
-        pkgs.gh
-        pkgs.gnupg
-        pkgs.iperf
-        pkgs.lua-language-server
-        pkgs.mkalias
-        pkgs.neovim
-        pkgs.nil
-        pkgs.obsidian
-        pkgs.opentofu
-        pkgs.pass
-        pkgs.postgresql_16
-        pkgs.rclone
-        pkgs.ripgrep
-        pkgs.rustup
-        pkgs.stylua
-        pkgs.unstable.stripe-cli
-        pkgs.tailwindcss
-        pkgs.tailwindcss-language-server
-        pkgs.templ
-        pkgs.tmux
-        pkgs.unstable.amber-lang
-        pkgs.zoxide
-      ];
+      environment.systemPackages =
+        [
+          pkgs.alacritty
+          pkgs.air
+          pkgs.awscli
+          pkgs.bun
+          pkgs.ffmpeg
+          pkgs.git
+          pkgs.gh
+          pkgs.gnupg
+          pkgs.unstable.go_1_23
+          pkgs.iperf
+          pkgs.lua-language-server
+          pkgs.mkalias
+          pkgs.neovim
+          pkgs.nil
+          pkgs.obsidian
+          pkgs.opentofu
+          pkgs.pass
+          pkgs.postgresql_16
+          pkgs.rclone
+          pkgs.ripgrep
+          pkgs.rustup
+          pkgs.sqlc
+          pkgs.stylua
+          pkgs.unstable.stripe-cli
+          pkgs.tailwindcss
+          pkgs.tailwindcss-language-server
+          pkgs.templ
+          pkgs.tmux
+          pkgs.unstable.amber-lang
+          pkgs.zoxide
+        ];
 
       users.users.elliott = {
         name = username;
