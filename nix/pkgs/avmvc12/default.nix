8d66e9eaf6f80b179ae1a162d64cc76964cc5f14
Elliott Minns <elliott.minns@pm.me>
2024-10-17 21:06:58 -0500

    avmvc12 firmware added

---
+++ nix/pkgs/avmvc12/default.nix
@@ -0,0 +1,46 @@
+{
+  pkgs,
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  nukeReferences,
+}:
+stdenv.mkDerivation rec {
+  pname = "avmvc12-${version}-${pkgs.linuxPackages.kernel.version}";
+  version = "1.0";
+
+  src = fetchFromGitHub {
+    owner = "GloriousEggroll";
+    repo = "AVMATRIX-VC12-4K-CAPTURE";
+    rev = "main";
+    hash = "sha256-poAjXYQoMF9k0NyrHqdinOm2xjhEcyV0xdFP83z24cg=";
+  };
+
+  sourceRoot = "source/src";
+
+  hardeningDisable = ["pic"];
+
+  nativeBuildInputs = [nukeReferences] ++ [pkgs.linuxPackages.kernel.moduleBuildDependencies];
+
+  makeFlags = [
+    "KERNELRELEASE=${pkgs.linuxPackages.kernel.modDirVersion}"
+    "KERNELDIR=${pkgs.linuxPackages.kernel.dev}/lib/modules/${pkgs.linuxPackages.kernel.modDirVersion}/build"
+    "INSTALL_MOD_PATH=$(out)"
+  ];
+
+  installPhase = ''
+    mkdir -p $out/lib/modules/${pkgs.linuxPackages.kernel.modDirVersion}/drivers/misc
+    for x in $(find . -name '*.ko'); do
+      nuke-refs $x
+      cp $x $out/lib/modules/${pkgs.linuxPackages.kernel.modDirVersion}/drivers/misc/
+    done
+  '';
+
+  meta = with lib; {
+    description = "Kernel module for AVMATRIX VC12-4K PCIe video capture card with small modifications by GloriousEggroll (Thomas Crider).";
+    homepage = "https://www.avmatrix.com/products/vc12-4k-4k-hdmi-pcie-capture-card/";
+    license = licenses.gpl2;
+
+    platforms = platforms.linux;
+  };
+}
--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -1,5 +1,6 @@
 pkgs: {
   gptscript = pkgs.callPackage ./gptscript {};
   banana-cursor-dreams = pkgs.callPackage ./banana-cursor-dreams {};
+  avc12-4k-capture = pkgs.callPackage ./avmvc12 {};
   # example = pkgs.callPackage ./example { };
 }
