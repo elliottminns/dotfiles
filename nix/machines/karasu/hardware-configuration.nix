f29e637b939e64758548637f20dffbac8d349169
Elliott Minns <elliott.minns@pm.me>
2024-11-26 15:14:34 -0600

    Deep sleep added to karasu

--- nix/machines/karasu/hardware-configuration.nix
+++ nix/machines/karasu/hardware-configuration.nix
@@ -16,6 +16,7 @@
   boot.initrd.kernelModules = ["dm-snapshot"];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
+  boot.kernelParams = ["mem_sleep_default=deep"];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
