386f0145819c3c1801683b51478ff4d2d62447cf
Elliott Minns <elliott.minns@pm.me>
2025-08-19 22:52:31 -0500

    feat: zoxide for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,6 @@ if status is-interactive
   set -x GPG_TTY (tty)
   set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
   gpgconf --launch gpg-agent
+
+  zoxide init --cmd cd fish | source
 end
