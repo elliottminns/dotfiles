9ced1f8135d7ff5c96be2c62257f279f025eb046
Elliott Minns <elliott.minns@pm.me>
2026-01-09 11:12:13 -0600

    feat: blackmagic drivers

--- nix/machines/zenbox/hardware-configuration.nix
+++ nix/machines/zenbox/hardware-configuration.nix
@@ -15,7 +15,10 @@
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "thunderbolt" "usbhid" "uas" "sd_mod"];
   boot.initrd.kernelModules = ["dm-snapshot" "amdgpu"];
   boot.kernelModules = ["kvm-amd"];
-  boot.extraModulePackages = [ pkgs.blackmagic-desktop-video ];
+  boot.extraModulePackages = [];
+
+  # Blackmagic UltraStudio Mini support
+  hardware.decklink.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
