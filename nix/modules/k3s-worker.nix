3b0c04cc0ff0d95a279a290274f4927235182d37
Elliott Minns <elliott.minns@pm.me>
2026-05-16 10:37:10 -0500

    feat: setting node as k3s worker

--- nix/modules/k3s-worker.nix
+++ nix/modules/k3s-worker.nix
@@ -5,10 +5,60 @@
   ...
 }: let
   isZenbox = meta.hostname == "zenbox";
+  k3s_1_30_1 = pkgs.stdenvNoCC.mkDerivation rec {
+    pname = "k3s";
+    version = "1.30.1+k3s1";
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/k3s-io/k3s/releases/download/v1.30.1%2Bk3s1/k3s";
+      hash = "sha256-OaUFf7Sb9XakXDLvPvY7/0SCUtTSXGxBuNzF5I4ni/U=";
+    };
+
+    nativeBuildInputs = [pkgs.makeWrapper];
+
+    runtimeDeps = with pkgs; [
+      kmod
+      socat
+      iptables
+      nftables
+      iproute2
+      ipset
+      bridge-utils
+      ethtool
+      util-linuxMinimal
+      conntrack-tools
+      runc
+      bash
+      shadow
+    ];
+
+    dontUnpack = true;
+
+    installPhase = ''
+      runHook preInstall
+
+      install -Dm755 $src $out/bin/k3s
+      wrapProgram $out/bin/k3s \
+        --prefix PATH : ${lib.makeBinPath runtimeDeps} \
+        --prefix PATH : "$out/bin"
+      ln -s k3s $out/bin/kubectl
+      ln -s k3s $out/bin/crictl
+      ln -s k3s $out/bin/ctr
+
+      runHook postInstall
+    '';
+
+    meta =
+      pkgs.k3s.meta
+      // {
+        sourceProvenance = [lib.sourceTypes.binaryNativeCode];
+      };
+  };
 in {
   config = lib.mkIf isZenbox {
     services.k3s = {
       enable = true;
+      package = k3s_1_30_1;
       role = "agent";
       serverAddr = "https://homelab-0:6443";
       tokenFile = "/etc/rancher/k3s/cluster-token";
@@ -25,7 +75,7 @@ in {
     };
 
     environment.systemPackages = with pkgs; [
-      k3s
+      k3s_1_30_1
       cifs-utils
       nfs-utils
     ];
